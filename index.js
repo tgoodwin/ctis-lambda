125275b294069a5cccd88a962fc0a9fcf706fd25
Tim Goodwin <tgoodwin@graphiq.com>
2016-08-02 13:47:16 -0700

    Lambda: exit on return call to callback to have Lambda func return data

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var childProcess = require('child_process');
 var path = require('path');
 
-exports.handler = function(event, context) {
-	console.log('HELLO');
+exports.handler = function(event, context, callback) {
 
 	// Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
 	process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
@@ -16,20 +15,28 @@ exports.handler = function(event, context) {
 		'my arg'
 	];
 	
+	var stderr = '';
+	var stdout = '';
+
 	// Launch the child process
 	console.log('calling phantom: ', phantomPath, processArgs);
 	var ls = childProcess.execFile(phantomPath, processArgs);
 	
 	ls.stdout.on('data', function(data) {
+		stdout += data;
 		console.log(data);
 	});
 
 	ls.stderr.on('data', function(data) {
+		stderr += data;
 		console.log('phantom error: ' + data);
 	});
 
 	ls.on('exit', function(code) {
 		console.log('child process exited with code ' + code);
-		context.done();
+		if (code !== 0) {
+			return callback(true, stderr);
+		}
+		return callback(null, stdout);
 	});
 }
--- rasterize.js
+++ rasterize.js
@@ -1,8 +1,13 @@
 // phantom script
 
 var system = require('system');
+var page = require('webpage').create();
+
 var env = system.env;
 var args = system.args;
+
+page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+	+ 'Chrome/37.0.2062.120 Safari/537.36 FTBImageGenerator/1.0';
 
 var unusedArg = args[1]; // my arg
 
