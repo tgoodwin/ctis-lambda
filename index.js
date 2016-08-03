2994b3fa7f9c956a2bf901c8a4552449a76ede29
Tim Goodwin <tgoodwin@graphiq.com>
2016-08-02 18:38:06 -0700

    rasterize: successfully screenshotting URLs, encoding them to base64 strings and passing back to Lambda thru stdout

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
 var childProcess = require('child_process');
 var path = require('path');
+var AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
 
 	// Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
 	process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+
+	// SET VALUES PASSED IN FROM API CALL
+	var url = event.url || 'https://w.graphiq.com/w/hbegDLDcAnj';
+	var format = (!!event.format) ? event.format : 'png';
+	var size = !!(event.size) ? event.size : '';
 	
+	var childArgs = [
+		path.join(__dirname, 'rasterize.js'),
+		url
+	];
+
+	/* Grabbing the screen */
+
 	// Set the path to the phantomjs binary
 	var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
 
 	// Arguments for the phantom script
-	var processArgs = [
-		path.join(__dirname, 'rasterize.js'),
-		'my arg'
-	];
 	
 	var stderr = '';
 	var stdout = '';
 
 	// Launch the child process
-	console.log('calling phantom: ', phantomPath, processArgs);
-	var ls = childProcess.execFile(phantomPath, processArgs);
+	console.log('calling phantom with params: ', phantomPath, childArgs);
+	var child = childProcess.execFile(phantomPath, childArgs, function(error, stdout, stderr) {
+		if (error !== null) {
+			context.fail(error);
+		}
+	});
 	
-	ls.stdout.on('data', function(data) {
+	child.stdout.on('data', function(data) {
 		stdout += data;
 		console.log(data);
 	});
 
-	ls.stderr.on('data', function(data) {
+	child.stderr.on('data', function(data) {
 		stderr += data;
 		console.log('phantom error: ' + data);
 	});
 
-	ls.on('exit', function(code) {
+	child.on('exit', function(code) {
 		console.log('child process exited with code ' + code);
 		if (code !== 0) {
-			return callback(true, stderr);
+			return callback('it broke', stderr);
 		}
+		// var buffer = new Buffer(stdout);
+		// return s3_upload_image(buffer, options);
 		return callback(null, stdout);
 	});
 }
--- rasterize.js
+++ rasterize.js
@@ -4,13 +4,30 @@ var system = require('system');
 var page = require('webpage').create();
 
 var env = system.env;
-var args = system.args;
 
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
 	+ 'Chrome/37.0.2062.120 Safari/537.36 FTBImageGenerator/1.0';
 
-var unusedArg = args[1]; // my arg
+var url = system.args[1];
 
-system.stdout.write('hello from phantom!');
+// system.stdout.write('hello from phantom!');
 
-phantom.exit(0);
+page.onResourceReceived = function(response) {
+	if (response.status >= 400) {
+		system.stderr.write('fail!!!' + response.status);
+		phantom.exit(1);
+	}
+};
+
+page.open(url, function (status) {
+	if (status !== 'success') {
+		system.stderr.write('Error opening url');
+		phantom.exit(1);
+	}
+
+	window.setTimeout(function() {
+		var base64 = page.renderBase64('PNG');
+		system.stdout.write(base64);
+		phantom.exit(0); // we are done here!
+	}, 1000);
+});
