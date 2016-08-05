34eaa89e86ede3cc7501e81603f2c5f27d1b338e
Tim Goodwin <tgoodwin@graphiq.com>
2016-08-05 11:51:23 -0700

    parameter handling but something broken now

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var s3_upload_image = function(img_data, path, callback) {
 
 	s3.upload(params, function(err, data) {
 		if (err) {
-			callback(err, err.message);
+			callback(err);
 		} else {
 			var bucketUrl = genS3Url(params.Bucket, params.Key);
 			callback(null, {'s3Url': bucketUrl});
@@ -26,7 +26,12 @@ var s3_upload_image = function(img_data, path, callback) {
 	});
 }
 
-// assumes S3 bucket being statically hosted
+var formatInputUrl = function(url) {
+	if ((url.indexOf('http://') <= -1) && ((url.indexOf('https://') <= -1)))
+		return 'https://' + url;
+}
+
+// assumes S3 bucket statically hosted with valid website endpoint
 var genS3Url = function(bucket, key) {
 	return 'https://' + bucket + '/' + key;
 }
@@ -37,27 +42,43 @@ exports.handler = function(event, context, callback) {
 	// https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
 	process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-	// SET VALUES PASSED IN FROM API CALL
-	var url = 'https://w.graphiq.com/w/hbegDLDcAnj';
-	var format = (!!event.format) ? event.format : 'png';
-	var size = !!(event.size) ? event.size : '';
-	var filename = sha1(url) + '.' + 'png';
+	event.url = 'https://w.graphiq.com/w/hbegDLDcAnj';
+	// fail fast before executing phantom binary
+	if (!event.url) {
+		console.error('No content url provided.');
+		return callback('Error: No content url provided');
+	}
+	// set values from the event object (what's passed in from the POST request body)
+	// var url = event.url ? event.url : 'https://w.graphiq.com/w/hbegDLDcAnj';
+	var format = (event.format === 'jpg') ? 'jpg' : 'png';
+	var size = event.size ? event.size : '';
+	var filename = sha1(event.url) + '.' + format; // dont use filenamefull
+
+
+	// not gonna bother with the require('optimist') stuff
+	// nobody's gonna be passing in params from the command line with their fingers
+	var phantomParams = {
+		url: formatInputUrl(event.url),
+		size: event.size,
+		format: event.format,
+		zoom: event.zoom ? event.zoom : 1
+	};
+	console.log(phantomParams);
 	
 	var childArgs = [
 		path.join(__dirname, 'rasterize.js'),
-		url
+		phantomParams
 	];
 
 	// Set the path to the phantomjs binary
 	var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
 
-	
 	var stderr = '';
 	var stdout = '';
 
 	// Launch PhantomJS as a child process
 	console.log('calling phantom with params: ', phantomPath, childArgs);
-	var child = childProcess.execFile(phantomPath, childArgs, function(error, stdout, stderr) {
+	var child = childProcess.execFile(phantomPath, childArgs, {maxBuffer: 1024 * 4096}, function(error, stdout, stderr) {
 		if (error !== null) {
 			console.log('Error executing Phantom process: ', error.message);
 			context.fail(error);
@@ -72,17 +93,16 @@ exports.handler = function(event, context, callback) {
 
 	child.stderr.on('data', function(data) {
 		stderr += data;
-		console.log('phantom error: ' + data);
+		console.log('phantom stderr: ' + data);
 	});
 
 	child.on('exit', function(code) {
 		console.log('child process exited with code ' + code);
 		if (code !== 0) {
-			return callback(stderr, 'phantom error: ');
+			return callback(stderr, 'phantom error');
 		}
-		// DEPRECATED as of Node 6.0!!!
+		// NOTE: deprecated as of Node 6.0
 		var buffer = new Buffer(stdout, 'base64');
 		return s3_upload_image(buffer, filename, callback);
-		// return callback(null, stdout);
 	});
 }
--- rasterize.js
+++ rasterize.js
@@ -4,20 +4,34 @@ var system = require('system');
 var page = require('webpage').create();
 
 var env = system.env;
+var imgFormat = 'PNG';
 
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
 	+ 'Chrome/37.0.2062.120 Safari/537.36 FTBImageGenerator/1.0';
 
+// object with keys url, size, and zoom
+var args = system.args[1];
 var defaultViewport = {width: 1440, height: 900};
 page.viewportSize = defaultViewport;
-page.zoomFactor = 1;
 
-var url = system.args[1];
+var argWidth, argHeight;
+if (args.size) {
+	var size = ('' + args.size).split('*');
+	argWidth = size[0] ? parseInt(size[0], 10) : null;
+	argHeight = size[1] ? parseInt(size[1], 10) : null;
+	page.viewportSize = {
+		width: defaultViewport.width,
+		height: defaultViewport.height
+	};
+}
+
+imgFormat = (args.format === 'jpg') ? 'JPEG' : 'PNG';
+page.zoomFactor = args.zoom || 1;
 
 var renderAndExit = function() {
 	var contentWidth = page.evaluateJavaScript('function() {return document.body.firstElementChild.offsetWidth;}');
 	var contentHeight = page.evaluateJavaScript('function() {return document.body.firstElementChild.offsetHeight;}');
-	
+
 	page.clipRect = {
 		top: 0,
 		left: 0,
@@ -27,23 +41,26 @@ var renderAndExit = function() {
 
 	// wait a second for the page to load before screenshotting
 	window.setTimeout(function() {
-		var base64 = page.renderBase64('PNG');
+		var base64 = page.renderBase64(imgFormat);
 		system.stdout.write(base64);
 		phantom.exit(0);
 	}, 1000);
 };
+
 page.onResourceReceived = function(response) {
 	if (response.status >= 400) {
 		system.stderr.write('Phantom: resource failed with status code: ' + response.status);
 		phantom.exit(1);
 	}
 };
+
+// help us see potential server-side errors in the Lambda console
 page.onResourceError= function(resourceError) {
 	page.reason = resourceError.errorString;
 	page.reason_url = resourceError.url;
 };
 
-page.open(url, function (status) {
+page.open(args.url, function (status) {
 	if (status !== 'success') {
 		system.stderr.write('Error opening url \'' + page.reason_url + '\': ' + page.reason);
 		phantom.exit(1);
